f885d0a4315eb39aba99059d8d51a6ffd8507f06
Alihan <220316034@ogr.cbu.edu.tr>
2025-09-27 18:44:50 +0300

    OrderedDict added

---
+++ CollectionModule/OrderedDict.py
@@ -0,0 +1,18 @@
+from collections import OrderedDict
+print("Before deleting: \n")
+od = OrderedDict()
+od['a'] = 1
+od['b'] = 2
+od['c'] = 3
+od['d'] = 4
+od['f'] = 5
+for key, value in od.items():
+    print(key,value)
+print("\n After deleting:\n")
+od.pop('c')
+for key,value in od.items():
+    print(key,value)
+print("\n After re-instering : \n")
+od['c'] = 3
+for key,value in od.items():
+    print(key,value)
