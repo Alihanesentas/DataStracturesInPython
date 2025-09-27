0eb4a9e27a311a4f35d6e3942a7b4de564a4df21
Alihan <220316034@ogr.cbu.edu.tr>
2025-09-27 19:40:58 +0300

    UserDict

---
+++ CollectionModule/UserDict.py
@@ -0,0 +1,14 @@
+from collections import UserDict
+
+class MyDict(UserDict):
+    def __del__(self):
+        raise RuntimeError("Deletion not allowed")
+    def pop(self,s=None):
+        raise RuntimeError("Deletion not allowed")
+    def popitem(self,s=None):
+        raise RuntimeError("Deletion not allowed")
+d = MyDict({'a':1,
+                'b':2,
+                'c':3})
+print(d)    
+d.pop(1)
